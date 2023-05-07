31aaf55b84199e2e962e8246379cf7eee5de9368
aa <71987344+sturdybinder@users.noreply.github.com>
2023-05-07 00:42:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,71 @@
 let playerScore = 0;
 let computerScore = 0;
 
-let playerName = prompt("Hello! Welcome to Rock, Paper, Scissors! What is your name?");
-
 function computerPlay() {
-  let selection = ["rock", "paper", "scissors"];
-  let randomIndex = Math.floor(Math.random() * selection.length);
-  return selection[randomIndex];
+  const choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
-  try {
-    if (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
-      throw new Error("Invalid input!");
+  while (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
+    playerSelection = prompt(`${playerName}, that's an invalid input. Please enter Rock, Paper, or Scissors.`)
+      .trim()
+      .toLowerCase();
+    if (playerSelection === null) {
+      console.log("Round cancelled.");
+      return;
     }
-
-    if (playerSelection === computerSelection) {
-      console.log("It's a tie! The computer also picked " + computerSelection + ".");
-    } else if (
-      (playerSelection === "rock" && computerSelection === "scissors") ||
-      (playerSelection === "paper" && computerSelection === "rock") ||
-      (playerSelection === "scissors" && computerSelection === "paper")
-    ) {
-      console.log("You win! " + playerSelection + " beats " + computerSelection + " " + computerSelection + ".");
-      playerScore++;
-    } else {
-      console.log("You lose! " + computerSelection + " beats " + playerSelection + " " + computerSelection + ".");
-      computerScore++;
-    }
-  } catch (err) {
-    console.log(err.message);
+  }
+  
+  console.log(`You chose ${playerSelection}.`);
+  console.log(`Computer chose ${computerSelection}.`);
+  
+  if (playerSelection === computerSelection) {
+    console.log("It's a tie!");
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    console.log(`You win this round! ${playerSelection} beats ${computerSelection}.`);
+    playerScore++;
+  } else {
+    console.log(`You lose this round! ${computerSelection} beats ${playerSelection}.`);
+    computerScore++;
   }
 }
 
 function game() {
-  console.log(`Welcome ${playerName} to Rock, Paper, Scissors!`);
-  for (let i = 1; i <= 5; i++) {
-    console.log(`Round ${i}:`);
-    let playerSelection = prompt(`${playerName}, enter Rock, Paper, or Scissors.`);
+  console.log(`Welcome to Rock, Paper, Scissors, ${playerName}!`);
+  
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("Enter Rock, Paper, or Scissors.")
+      .trim()
+      .toLowerCase();
     if (playerSelection === null) {
       console.log("Game cancelled.");
       return;
     }
-    playerSelection = playerSelection.trim().toLowerCase();
-    let computerSelection = computerPlay();
+    
+    const computerSelection = computerPlay();
     playRound(playerSelection, computerSelection);
+    
     console.log(`Current score: ${playerName} ${playerScore} - ${computerScore} Computer`);
   }
-
+  
+  console.log(`Final score: ${playerName} ${playerScore} - ${computerScore} Computer`);
   if (playerScore > computerScore) {
-    console.log(`Congratulations ${playerName}! You win! Final score: ${playerScore} - ${computerScore}`);
-  } else if (playerScore < computerScore) {
-    console.log(`Sorry ${playerName}, you lose! Final score: ${playerScore} - ${computerScore}`);
+    console.log(`${playerName} wins the game!`);
+  } else if (computerScore > playerScore) {
+    console.log("Computer wins the game!");
   } else {
-    console.log(`It's a tie! Final score: ${playerScore} - ${computerScore}`);
+    console.log("It's a tie game!");
   }
 }
 
-game();
+const playerName = prompt("Please enter your name.");
+if (playerName === null) {
+  console.log("Game cancelled.");
+} else {
+  game();
+}
